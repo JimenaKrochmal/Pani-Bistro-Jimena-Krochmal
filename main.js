f2a401bec81482efd876cb9ce245973f367d325e
Jimena Krochmal <jimenakrochmal@gmail.com>
2023-07-21 11:42:02 -0300

    agrego un else if a mi pre entrega uno

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ function pistasTortaIngresada(tortaIngresada){
     else if(tortaIngresada === "Cheesecake"){
         alert("La torta ingresada no es la correcta. PISTA: La torta no lleva galletitas oreos.");
     }
-    else{
+    else if (tortaIngresada === "Lemonpie"){
         alert("La torta ingresada no es la correcta. PISTA: La torta no es de queso.");
+    }
+    else{
+        alert("Lo ingresado debe encontrarse entre una de las opciones dichas anteriormente.");
     }
 }
 
