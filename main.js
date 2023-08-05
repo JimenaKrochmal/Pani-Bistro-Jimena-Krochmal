acb33074679a4c2d9ba4806a904ccc79a73c4bf9
Jimena Krochmal <jimenakrochmal@gmail.com>
2023-08-05 17:16:34 -0400

    cambios en la segunda pre entrega

--- main.js
+++ main.js
@@ -28,6 +28,54 @@
 
 //SEGUNDA PRE- ENTREGA:
 
+// const tortas = [
+//     {nombre: "Chocotorta", precio: 100, stock: 20 },
+//     {nombre: "Oreo", precio: 601, stock: 300 },
+//     {nombre: "Lemonpie", precio: 1000, stock: 0},
+//     {nombre: "Cheesecake", precio: 2000, stock: 50}
+// ];
+
+// let tortaIngresada = prompt("Ingrese el nombre de la torta que desea buscar: ");
+
+// const findTorta = tortas.find(item => item.nombre === tortaIngresada);
+
+// function datosTorta(findTorta){
+//     alert(`
+//     los datos de la torta que usted ingresó son los siguientes: 
+//     Nombre: ${findTorta.nombre}
+//     Precio: $ ${findTorta.precio}
+//     cantidad de stock: ${findTorta.stock} unidades
+//     `)
+// }
+
+// if(findTorta){
+//     datosTorta(findTorta);
+// }else{
+//     let agregarTorta = prompt("¿Desea agregarla? Si/ No : ");
+//     if(agregarTorta === "Si"){
+//         class TortaAgregada{
+//             constructor(nombreTorta, precioTorta, stockTorta){
+//                 this.nombre = nombreTorta;
+//                 this.precio = precioTorta;
+//                 this.stock = stockTorta;
+//             }
+//         }
+
+//         let nombreTortaIngresada = prompt("ingrese el nombre de la torta: ");
+//         let precioTortaIngresada = parseInt(prompt("ingrese el precio de la torta: "));
+//         let stockTortaIngresada = parseInt(prompt("ingrese el stock de la torta: "));
+
+//         const tortaIngreso = new TortaAgregada(nombreTortaIngresada, precioTortaIngresada, stockTortaIngresada);
+//         tortas.push(tortaIngreso);
+
+//         const nombresTortas = tortas.map((item) => item.nombre);
+//         alert("La torta fue cargada. Todas las tortas hasta ahora: " + nombresTortas);
+//     }else{
+//         alert("Ok. Hasta luego.");
+//     }
+// }
+
+
 const tortas = [
     {nombre: "Chocotorta", precio: 100, stock: 20 },
     {nombre: "Oreo", precio: 601, stock: 300 },
@@ -35,38 +83,40 @@ const tortas = [
     {nombre: "Cheesecake", precio: 2000, stock: 50}
 ];
 
-let tortaIngresada = prompt("Ingrese el nombre de la torta que desea buscar: ");
+let comenzar = prompt("¿desea comenzar? Si/ No");
 
-const findTorta = tortas.find(item => item.nombre === tortaIngresada);
-
-function datosTorta(findTorta){
+while(comenzar === "Si"){
+    let tortaBuscar = prompt("Ingrese el nombre de la torta que desea buscar: ");
+    const findTorta = tortas.find(item => item.nombre === tortaBuscar);
+    
+    function datosTorta(findTorta){
     alert(`
     los datos de la torta que usted ingresó son los siguientes: 
     Nombre: ${findTorta.nombre}
     Precio: $ ${findTorta.precio}
     cantidad de stock: ${findTorta.stock} unidades
     `)
-}
-
-if(findTorta){
-    datosTorta(findTorta);
-}else{
-    let agregarTorta = prompt("¿Desea agregarla? Si/ No : ");
-    if(agregarTorta === "Si"){
-        class TortaAgregada{
-            constructor(nombreTorta, precioTorta, stockTorta){
-                this.nombre = nombreTorta;
-                this.precio = precioTorta;
-                this.stock = stockTorta;
+    }
+    
+    if(findTorta){
+        datosTorta(findTorta);
+    }else{
+        let agregarTorta = prompt("¿Desea agregarla? Si/ No : ");
+        if(agregarTorta === "Si"){
+            class TortaAgregada{
+                constructor(nombreTorta, precioTorta, stockTorta){
+                    this.nombre = nombreTorta;
+                    this.precio = precioTorta;
+                    this.stock = stockTorta;
+                }
             }
-        }
-
-        let nombreTortaIngresada = prompt("ingrese el nombre de la torta: ");
-        let precioTortaIngresada = parseInt(prompt("ingrese el precio de la torta: "));
-        let stockTortaIngresada = parseInt(prompt("ingrese el stock de la torta: "));
-
-        const tortaIngreso = new TortaAgregada(nombreTortaIngresada, precioTortaIngresada, stockTortaIngresada);
-        tortas.push(tortaIngreso);
+            
+            let nombreTortaIngresada = prompt("ingrese el nombre de la torta: ");
+            let precioTortaIngresada = parseInt(prompt("ingrese el precio de la torta: "));
+            let stockTortaIngresada = parseInt(prompt("ingrese el stock de la torta: "));
+            
+            const tortaBuscar = new TortaAgregada(nombreTortaIngresada, precioTortaIngresada, stockTortaIngresada);
+            tortas.push(tortaBuscar);
 
         const nombresTortas = tortas.map((item) => item.nombre);
         alert("La torta fue cargada. Todas las tortas hasta ahora: " + nombresTortas);
@@ -75,3 +125,5 @@ if(findTorta){
     }
 }
 
+
+}
