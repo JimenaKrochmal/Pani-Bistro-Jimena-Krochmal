859b431778d35d321bf8eea89d4c4056643b2e82
Jimena Krochmal <jimenakrochmal@gmail.com>
2023-08-08 16:28:26 -0400

    Borré algo que me olvidé de borrar

--- main.js
+++ main.js
@@ -28,53 +28,6 @@
 
 //SEGUNDA PRE- ENTREGA:
 
-// const tortas = [
-//     {nombre: "Chocotorta", precio: 100, stock: 20 },
-//     {nombre: "Oreo", precio: 601, stock: 300 },
-//     {nombre: "Lemonpie", precio: 1000, stock: 0},
-//     {nombre: "Cheesecake", precio: 2000, stock: 50}
-// ];
-
-// let tortaIngresada = prompt("Ingrese el nombre de la torta que desea buscar: ");
-
-// const findTorta = tortas.find(item => item.nombre === tortaIngresada);
-
-// function datosTorta(findTorta){
-//     alert(`
-//     los datos de la torta que usted ingresó son los siguientes: 
-//     Nombre: ${findTorta.nombre}
-//     Precio: $ ${findTorta.precio}
-//     cantidad de stock: ${findTorta.stock} unidades
-//     `)
-// }
-
-// if(findTorta){
-//     datosTorta(findTorta);
-// }else{
-//     let agregarTorta = prompt("¿Desea agregarla? Si/ No : ");
-//     if(agregarTorta === "Si"){
-//         class TortaAgregada{
-//             constructor(nombreTorta, precioTorta, stockTorta){
-//                 this.nombre = nombreTorta;
-//                 this.precio = precioTorta;
-//                 this.stock = stockTorta;
-//             }
-//         }
-
-//         let nombreTortaIngresada = prompt("ingrese el nombre de la torta: ");
-//         let precioTortaIngresada = parseInt(prompt("ingrese el precio de la torta: "));
-//         let stockTortaIngresada = parseInt(prompt("ingrese el stock de la torta: "));
-
-//         const tortaIngreso = new TortaAgregada(nombreTortaIngresada, precioTortaIngresada, stockTortaIngresada);
-//         tortas.push(tortaIngreso);
-
-//         const nombresTortas = tortas.map((item) => item.nombre);
-//         alert("La torta fue cargada. Todas las tortas hasta ahora: " + nombresTortas);
-//     }else{
-//         alert("Ok. Hasta luego.");
-//     }
-// }
-
 
 const tortas = [
     {nombre: "Chocotorta", precio: 100, stock: 20 },
